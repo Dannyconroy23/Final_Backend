6c22ee454007fb2ef0b69cea6a43216138f2ef65
Daniel Conroy <dannyconroy23@hotmail.com>
2022-06-09 18:34:54 -0400

    signin complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :show, :create]
 
-  # get '/authorized_user', to: 'users#show'
+  get 'authorized_user', to: 'users#show'
 
   
 
